75ba1b5541d48f725a594ed89490f45ac6c576b1
Richard Tan <chardos@gmail.com>
2016-02-24 21:23:32 +1100

    Emitting the chat message

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.get('/', function(req, res){
 
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
@@ -18,3 +17,5 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
